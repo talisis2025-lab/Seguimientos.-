df736d1dbcc8b0d5876aa622e6083925fa22cfcd
talisis2025-lab <talisis2025@gmail.com>
2026-08-14 11:45:26 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Opcional: pega aquí la URL de Apps Script para que quede incluida en GitHub.
+// También puedes configurarla desde el engrane de la página; se guardará en tu navegador.
+window.APP_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbxTrB1bl2tQjm31TwYY-5KPg8XbuSeGaVbrijWAvS86jBg07XdZmODIXKyIS-wzNK3c/exec"
+};
